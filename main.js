8d6c46ce0f781cc4c8df4b1e0bbdfdfbd29a2d69
Peter <pmhurter@gmail.com>
2017-12-01 09:44:40 +0000

    Provide for click/touch screen inter action

--- main.js
+++ main.js
@@ -8,6 +8,15 @@ function playSound(e){
     key.classList.add('playing');
 }
 
+function playClickSound(e){
+    const audio = document.querySelector(`audio[data-key="${e}"`);
+    const key = document.querySelector(`.key[data-key="${e}"`);
+    if (!audio) return;
+    audio.currentTime = 0;
+    audio.play();
+    key.classList.add('playing');
+}
+
 function removeTransition(e) {
     if(e.propertyName !== 'transform') return;
     this.classList.remove('playing');
@@ -15,4 +24,6 @@ function removeTransition(e) {
 
 const keys = document.querySelectorAll('.key');
 keys.forEach(key => key.addEventListener('transitionend', removeTransition));
-window.addEventListener('keydown',playSound);
+window.addEventListener('keydown', playSound);
+window.addEventListener('click')
+window.getElementsByClassName('key').addEventListener('click', playClickSound ); 
